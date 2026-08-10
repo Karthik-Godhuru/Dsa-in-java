cd4da886f3cdfb903b3df1bd610b7230341158eb
KARTHIK GODHURU <karthikdevanga001@gmail.com>
2026-08-10 22:08:13 +0530

    Create 05_LeftRotateArrayByOne.java Day 5: Left Rotate Array by One Place

---
+++ Arrays/05_LeftRotateArrayByOne.java
@@ -0,0 +1,18 @@
+public class LeftRotateArrayByOne {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 4, 5};
+
+        int temp = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            arr[i - 1] = arr[i];
+        }
+
+        arr[arr.length - 1] = temp;
+
+        for (int j = 0; j < arr.length; j++) {
+            System.out.print(arr[j] + "  ");
+        }
+    }
+}
