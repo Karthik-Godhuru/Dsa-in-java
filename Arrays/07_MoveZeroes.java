6019c8a7b36346201281a4594dd2f96e4104daa3
KARTHIK GODHURU <karthikdevanga001@gmail.com>
2026-08-18 00:28:05 +0530

    Create 07_MoveZeroes.java

---
+++ Arrays/07_MoveZeroes.java
@@ -0,0 +1,31 @@
+public static void swap(int[] arr, int i, int j) {
+int temp = arr[i];
+arr[i] = arr[j];
+arr[j] = temp;
+}
+public static void main( String[] args)
+{
+int[] arr={ 1,0,2,3,2,0,0,4,5,1};
+int n = arr.length;
+int j=-1;
+for(int i =0; i<n;i++)
+{
+if(arr[i]==0)
+{
+j=i;
+break;
+}
+}
+for(int i=j+1;i<n;i++)
+{
+if(arr[i]!=0)
+{
+swap(arr,i,j);
+j++;
+}
+}
+for(int i=0;i<n;i++)
+{
+System.out.print(arr[i]+" ");
+}
+}
