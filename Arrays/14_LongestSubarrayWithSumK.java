140c1963dc22604ba4aab050b7a8cf8bee2d7ecb
KARTHIK GODHURU <karthikdevanga001@gmail.com>
2026-09-16 23:19:14 +0530

    Create 14_LongestSubarrayWithSumK.java Add longest subarray with sum K solution

---
+++ Arrays/14_LongestSubarrayWithSumK.java
@@ -0,0 +1,27 @@
+public static void main(String [] args)
+    {
+    int[] arr={1,2,3,1,1,1,1,3,3};
+    int n =arr.length;
+    int right = 0;
+    int left = 0;
+    int k = 6;
+    int maxlen = 0;
+    int sum = arr[right];
+    while (right<n)
+        {
+            while (left<=right && sum > k)
+                {
+                    sum -=arr[left];
+                    left++;
+                }
+            if(sum == k)
+            {
+                maxlen=Math.max(maxlen,right - left + 1);
+            }
+            right++;
+            if(right<n)
+                sum+=arr[right];
+        }
+    System.out.print(maxlen);
+}
+    
